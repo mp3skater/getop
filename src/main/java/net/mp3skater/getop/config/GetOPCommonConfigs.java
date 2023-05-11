f7145f4841b95f3cba957d928ee64ebc5257547e
Matteo Garbin <mp3skaterr@gmail.com>
2023-05-11 19:47:27 +0200

    DeathSwordItem lists all entities in configurable radius

--- src/main/java/net/mp3skater/getop/config/GetOPCommonConfigs.java
+++ src/main/java/net/mp3skater/getop/config/GetOPCommonConfigs.java
@@ -7,13 +7,17 @@ public class GetOPCommonConfigs {
     public static final ForgeConfigSpec SPEC;
 
     public static final ForgeConfigSpec.ConfigValue<Double> END_SCEPTRE_REACH_DISTANCE;
+
+    public static final ForgeConfigSpec.IntValue DEATH_SWORD_REACH_DISTANCE;
     public static final ForgeConfigSpec.ConfigValue<Integer> PAINITE_ORE_VEIN_SIZE;
 
     static {
         BUILDER.push("Configs for the Get OP Mod");
 
-        END_SCEPTRE_REACH_DISTANCE = BUILDER.comment("How long the reach distance of the teleport function!")
-                .defineInRange("Reach Distance", 50d, 6d, 100d);
+        END_SCEPTRE_REACH_DISTANCE = BUILDER.comment("How long the reach distance of the teleport ability!")
+                .defineInRange("Reach Distance", 10d, 4d, 100d);
+        DEATH_SWORD_REACH_DISTANCE = BUILDER.comment("How long the reach distance of death-ray ability!")
+                .defineInRange("Reach Distance", 50, 8, 200);
         PAINITE_ORE_VEIN_SIZE = BUILDER.comment("How much Painite spawns in your world!")
                 .defineInRange("Veins per Chunk(1 is really rare!)", 3, 1, 10);
 
--- src/main/java/net/mp3skater/getop/item/custom/DeathSwordItem.java
+++ src/main/java/net/mp3skater/getop/item/custom/DeathSwordItem.java
@@ -1,11 +1,24 @@
 package net.mp3skater.getop.item.custom;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.Tier;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.AABB;
+import net.mp3skater.getop.config.GetOPCommonConfigs;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+import java.util.List;
 
 public class DeathSwordItem extends SwordItem {
     public DeathSwordItem(Tier pTier, int pAttackDamageModifier, float pAttackSpeedModifier, Properties pProperties) {
@@ -17,4 +30,23 @@ public class DeathSwordItem extends SwordItem {
         pTarget.addEffect(new MobEffectInstance(MobEffects.WITHER, 100, 15), pAttacker);
         return super.hurtEnemy(pStack, pTarget, pAttacker);
     }
+
+    @Override
+    public @NotNull InteractionResultHolder<ItemStack>
+    use(@NotNull Level level, Player player, @NotNull InteractionHand hand) {
+        Level world = player.getLevel();
+        //is required for the server to control the Players movement
+            player.hurtMarked = true;
+        BlockPos pos = player.getOnPos();
+        Entity entity = player;
+        int radius = GetOPCommonConfigs.DEATH_SWORD_REACH_DISTANCE.get();
+        List<Entity> entities = world.getEntities(entity, new AABB(pos.getX() - radius, pos.getY() - radius, pos.getZ() - radius, pos.getX() + radius, pos.getY() + radius, pos.getZ() + radius));
+        player.sendMessage(new TextComponent("In a radius of " + GetOPCommonConfigs.DEATH_SWORD_REACH_DISTANCE.get()
+                + " blocks these entities are present: " + entities), player.getUUID());
+
+        return super.use(level, player, hand);
+    }
+
+
+
 }
