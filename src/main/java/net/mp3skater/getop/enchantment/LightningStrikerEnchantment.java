22f0f909e8e6ffc0c919e1ea77a56f9e15136e2f
mp3skater <mp3skaterr@gmail.com>
2024-06-11 16:30:56 +0200

    fixed some small code lol

--- src/main/java/net/mp3skater/getop/enchantment/LightningStrikerEnchantment.java
+++ src/main/java/net/mp3skater/getop/enchantment/LightningStrikerEnchantment.java
@@ -1,17 +1,11 @@
 package net.mp3skater.getop.enchantment;
 
-import net.minecraft.core.BlockPos;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.*;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
-import net.minecraft.world.level.block.AirBlock;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockState;
 
 public class LightningStrikerEnchantment  extends Enchantment {
     public LightningStrikerEnchantment(Rarity pRarity, EnchantmentCategory pCategory, EquipmentSlot[] pApplicableSlots) {
         super(pRarity, pCategory, pApplicableSlots);
     }
-
 }
