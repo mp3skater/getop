531f1559816e88516817fa678b5ab1e6084b0c1e
mp3skater <mp3skaterr@gmail.com>
2024-07-08 22:15:25 +0200

    clean code

--- src/main/java/net/mp3skater/getop/event/ModEventBusEvents.java
+++ src/main/java/net/mp3skater/getop/event/ModEventBusEvents.java
@@ -1,11 +1,8 @@
 package net.mp3skater.getop.event;
 
-import net.minecraft.client.Minecraft;
 import net.minecraft.core.Registry;
 import net.minecraft.world.item.crafting.RecipeSerializer;
-import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraftforge.client.event.EntityRenderersEvent;
-import net.minecraftforge.client.event.ParticleFactoryRegisterEvent;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -14,8 +11,6 @@ import net.mp3skater.getop.GetOP;
 import net.mp3skater.getop.entity.ModEntityTypes;
 import net.mp3skater.getop.entity.client.armor.PainiteArmorRenderer;
 import net.mp3skater.getop.entity.custom.VoidShredderEntity;
-import net.mp3skater.getop.particle.ModParticles;
-import net.mp3skater.getop.particle.custom.Deathray_Particle;
 import net.mp3skater.getop.recipe.AnvilOfSageRecipe;
 import software.bernie.example.item.PotatoArmorItem;
 import software.bernie.geckolib3.renderers.geo.GeoArmorRenderer;
