4414737bb4a3a571554a6c4816263c0b9f70a440
mp3skater <mp3skaterr@gmail.com>
2024-09-01 19:45:21 +0200

    fixing: New world key not working #4

--- src/main/java/net/mp3skater/getop/item/custom/DimensionChangingItem.java
+++ src/main/java/net/mp3skater/getop/item/custom/DimensionChangingItem.java
@@ -1,5 +1,6 @@
 package net.mp3skater.getop.item.custom;
 
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
@@ -7,20 +8,26 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.phys.Vec3;
 import net.mp3skater.getop.util.ModUtils;
+import org.jetbrains.annotations.NotNull;
 
 public class DimensionChangingItem extends Item {
 	private final ResourceKey<Level> dimension;
+	private final Vec3 location;
 
-	public DimensionChangingItem(Properties pProperties, ResourceKey<Level> dimension) {
+	public DimensionChangingItem(Properties pProperties, ResourceKey<Level> dimension, Vec3 location) {
 		super(pProperties);
 		this.dimension = dimension;
+		this.location = location;
 	}
 
 	@Override
-	public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pUsedHand) {
+	public @NotNull InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pUsedHand) {
 		// Changing to GetOP Dimension
-		ModUtils.changeDimension(pPlayer, pLevel, dimension, this);
+		if(pPlayer instanceof ServerPlayer serverPlayer) {
+			ModUtils.teleportEntityToDimension(serverPlayer, dimension, location, this);
+		}
 
 		return super.use(pLevel, pPlayer, pUsedHand);
 	}
--- src/main/java/net/mp3skater/getop/world/dimension/ModDimensions.java
+++ src/main/java/net/mp3skater/getop/world/dimension/ModDimensions.java
@@ -9,7 +9,7 @@ import net.mp3skater.getop.GetOP;
 
 public class ModDimensions {
     public static final ResourceKey<Level> GTDIM_KEY = ResourceKey.create(Registry.DIMENSION_REGISTRY,
-            new ResourceLocation(GetOP.MOD_ID, "gtdim_key"));
+            new ResourceLocation(GetOP.MOD_ID, "gtdim"));
     public static final ResourceKey<DimensionType> GTDIM_TYPE =
             ResourceKey.create(Registry.DIMENSION_TYPE_REGISTRY, GTDIM_KEY.getRegistryName());
 
