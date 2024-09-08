cdb897721d6a7a866e2206b9a6d4b220af8661e5
mp3skater <mp3skaterr@gmail.com>
2024-09-08 17:39:39 +0200

    #12

--- src/main/java/net/mp3skater/getop/item/custom/PainiteArmorItem.java
+++ src/main/java/net/mp3skater/getop/item/custom/PainiteArmorItem.java
@@ -1,16 +1,14 @@
 package net.mp3skater.getop.item.custom;
 
-import net.minecraft.world.effect.MobEffect;
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ArmorMaterial;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.Level;
-import net.mp3skater.getop.effect.GetOPEffects;
-import net.mp3skater.getop.item.ModArmorMaterials;
+import net.minecraftforge.event.entity.living.LivingDamageEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.mp3skater.getop.item.ModItems;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.PlayState;
 import software.bernie.geckolib3.core.controller.AnimationController;
@@ -47,54 +45,61 @@ public class PainiteArmorItem extends GeoArmorItem implements IAnimatable, RareI
 		return PlayState.STOP;
 	}
 
-	@Override
-	public void onArmorTick(ItemStack stack, Level world, Player player) {
-		super.onArmorTick(stack, world, player);
-
-		// Return if not on server
-		if (world.isClientSide()) return;
-
-		// Give custom effects if you're wearing the armor
-		if (hasPieceOfArmorOn(player, 0) && hasCorrectArmorOn(player, 0))
-			add(player, GetOPEffects.GUM_SKIN_EFFECT.get());
-		if (hasPieceOfArmorOn(player, 1) && hasCorrectArmorOn(player, 1))
-			add(player, MobEffects.FIRE_RESISTANCE);
-		if (hasPieceOfArmorOn(player, 2) && hasCorrectArmorOn(player, 2))
-			add(player, GetOPEffects.HARDENED_SKIN_EFFECT.get());
-		if (hasPieceOfArmorOn(player, 3) && hasCorrectArmorOn(player, 3))
-			add(player, GetOPEffects.FEATHER_FALL_EFFECT.get());
-	}
-
-	private void add(Player player, MobEffect effect) {
-		if (!player.hasEffect(effect)) {
-			player.addEffect(new MobEffectInstance(effect, 200, 1));
-		}
-	}
-
-	private boolean hasPieceOfArmorOn(Player player, int slot) {
+	private static boolean hasNoArmorOn(Player player) {
 		ItemStack boots = player.getInventory().getArmor(0);
 		ItemStack leggings = player.getInventory().getArmor(1);
 		ItemStack breastplate = player.getInventory().getArmor(2);
 		ItemStack helmet = player.getInventory().getArmor(3);
 
-		return !helmet.isEmpty() && !breastplate.isEmpty()
-			&& !leggings.isEmpty() && !boots.isEmpty();
+		return helmet.isEmpty() && breastplate.isEmpty()
+			&& leggings.isEmpty() && boots.isEmpty();
 	}
 
-	private boolean hasCorrectArmorOn(Player player, int slot) {
+	@Mod.EventBusSubscriber
+	public static class ModEventBusEvents {
 
-		for (ItemStack armorStack : player.getInventory().armor) {
-			if (!(armorStack.getItem() instanceof ArmorItem)) {
-				return false;
+		@SubscribeEvent
+		public static void onLivingDamage(LivingDamageEvent event) {
+			if (event.getEntity() instanceof Player player) {
+				// Return if no armor on
+				if(hasNoArmorOn(player)) return;
+
+				// Helmet: Lightning protection
+				if (event.getSource() == DamageSource.LIGHTNING_BOLT) {
+					ItemStack helmet = player.getInventory().getArmor(3); // 3 = helmet slot
+					if (helmet.getItem() == ModItems.PAINITE_HELMET.get()) {
+						// Cancel lightning damage
+						event.setCanceled(true);
+					}
+				}
+
+				// Chestplate: Explosion protection
+				if (event.getSource().isExplosion()) {
+					ItemStack chestplate = player.getInventory().getArmor(2); // 2 = chestplate slot
+					if (chestplate.getItem() == ModItems.PAINITE_CHESTPLATE.get()) {
+						// Cancel explosion damage
+						event.setCanceled(true);
+					}
+				}
+
+				// Leggings: Fire protection
+				if (event.getSource().isFire() || event.getSource().isExplosion() || event.getSource().isBypassArmor()) {
+					ItemStack leggings = player.getInventory().getArmor(1); // 1 = leggings slot
+					if (leggings.getItem() == ModItems.PAINITE_LEGGINGS.get()) {
+						// Cancel fire damage
+						event.setCanceled(true);
+					}
+				}
+
+				// Boots: Cancel all damage when wearing boots
+				if(event.getSource().isFall()) {
+					ItemStack boots = player.getInventory().getArmor(0); // 0 = boots slot
+					if (boots.getItem() == ModItems.PAINITE_BOOTS.get()) {
+						// Cancel all damage
+						event.setCanceled(true);
+					}
+				}
 			}
 		}
-
-		ArmorItem boots = ((ArmorItem) player.getInventory().getArmor(0).getItem());
-		ArmorItem leggings = ((ArmorItem) player.getInventory().getArmor(1).getItem());
-		ArmorItem breastplate = ((ArmorItem) player.getInventory().getArmor(2).getItem());
-		ArmorItem helmet = ((ArmorItem) player.getInventory().getArmor(3).getItem());
-
-		return helmet.getMaterial() == ModArmorMaterials.PAINITE && breastplate.getMaterial() == ModArmorMaterials.PAINITE &&
-			leggings.getMaterial() == ModArmorMaterials.PAINITE && boots.getMaterial() == ModArmorMaterials.PAINITE;
 	}
 }
